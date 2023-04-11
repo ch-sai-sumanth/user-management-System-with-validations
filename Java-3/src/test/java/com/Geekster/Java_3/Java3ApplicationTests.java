a6c7e74eac13e92c6e9dc0a6921c5b62a86a33eb
saisumanth <saisumanth0531@gmail.com>
2023-04-11 18:27:38 +0530

    fetch data using java 3

---
+++ Java-3/src/test/java/com/Geekster/Java_3/Java3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Geekster.Java_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
