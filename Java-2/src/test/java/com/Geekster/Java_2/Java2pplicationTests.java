ba1b8eb436a29e43d7104c0b692d7bdb951bd52c
saisumanth <saisumanth0531@gmail.com>
2023-04-11 18:17:48 +0530

    fetch data using java-2

---
+++ Java-2/src/test/java/com/Geekster/Java_2/Java2pplicationTests.java
@@ -0,0 +1,13 @@
+package com.Geekster.Java_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2pplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
